92cb648617cb09d711830b72a9be59a52410c904
Viktor <viktor@INT-Viktor-Dabenskis-MacBook-Pro.local>
2024-01-22 16:14:51 +0200

    fixing remarks

--- src/main/java/eu/deltasource/training/library/config/WebMvcConfig.java
+++ src/main/java/eu/deltasource/training/library/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package eu.deltasource.training.library.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Value("${front.end.mapping.get.books}")
+    private String getBooksMapping;
+    @Value("${front.end.mapping.post.book}")
+    private String addBookMapping;
+    @Value("${front.end.origin}")
+    private String frontEndOrigin;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/book")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST");
+        registry.addMapping(getBooksMapping)
+                .allowedOrigins(frontEndOrigin)
+                .allowedMethods("GET");
+
+        registry.addMapping(addBookMapping)
+                .allowedOrigins(frontEndOrigin)
+                .allowedMethods("POST");
     }
 }
